ee5161eea272d9e6f86171f9cb277cc667fb36ef
Taniisha <65535848+Taniisha@users.noreply.github.com>
2020-11-07 14:45:38 +0530

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,55 +1,93 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Login Page</title>
-	<link rel="stylesheet" href="../css/style1.css">
-</head>
-<body>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="style.css" />
+    <title>Sign in & Sign up Form</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form" method="POST">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" name="log_username" required />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" name="log_password" required />
+            </div>
+            <input type="submit" value="Login" class="btn solid" name="login_submit" />
+          </form>
 
-	<div class="hero">
-		<div class="form-box">
-			<div class="button-box">
-				<div id="btn"></div>
-				<button class="toggle-btn" onclick="login()">Login</button>
-				<button class="toggle-btn" onclick="register()">Register</button>
-			</div>
-				<form id="login" class="input-group" action="#" method="POST">
-					<input type="text" class="input-field" placeholder="User Id" name="log_username" required>
-					<input type="password" class="input-field" placeholder="Enter Password" name="log_password" required>
-					<input type="checkbox" class="check-box"> <span>Remember Password</span>
-					<input type="submit" class="submit-btn" value="Login" name="login_submit" >
-				</form>
+          <form action="#" class="sign-up-form" method="POST">
+            <h2 class="title">Sign up</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" name="reg_username" required />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" name="email_id" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-phone-alt"></i>
+              <input type="tel" placeholder="Mobile number" name="mobile_no" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" name="reg_password" required/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-clipboard-check"></i>
+              <input type="password" placeholder="Confirm Password" name="confirm_password" required/>
+            </div>            
+            <input type="submit" class="btn" value="Sign up" name="register_submit" />
+          </form>
+        </div>
+      </div>
 
-				<form id="register" class="input-group" action="#" method="POST">
-					<input type="text" class="input-field" placeholder="User Id" name="reg_username" required>
-					<input type="password" class="input-field" placeholder="Enter Password" name="reg_password" required>
-					<input type="checkbox" class="check-box"> <span>I agree to the terms and conditions</span>
-					<input type="submit" class="submit-btn" value="Register" name="register_submit"  >
-				</form>
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>Not yet registered ?!</h3>
+            <p>
+              Register to book your slot for parking anytime.
+            </p>
+            <button class="btn transparent" id="sign-up-btn" >
+              Sign up
+            </button>
+          </div>
+          <br>
+          <img src="img/car.svg" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>Already registered ?</h3>
+            
+             <p>
+              Sign in now to book your parking slot!!
+            </p>
+            <button class="btn transparent" id="sign-in-btn">
+              Sign in
+            </button>
+          </div>
+          <img src="img/parking-man-sign.svg" class="image" alt="" />
+        </div>
+      </div>
+    </div>
 
-				
-			
+    <script src="app.js"></script>
+  </body>
 
-		</div>
-	</div>
-
-</body>
-<script>
-	var x= document.getElementById('login')
-	var y=document.getElementById('register')
-	var z=document.getElementById('btn')
-	const register=()=>{
-		x.style.left="-400px"
-		y.style.left="50px"
-		z.style.left="110px"
-	}
-	const login= () =>{
-		x.style.left="50px"
-		y.style.left="450px"
-		z.style.left="0px"
-	}
-</script>
-	<?php
+  <?php
 		session_start();
 		include('connection.php');
 		if($_SESSION['auth'])
@@ -80,7 +118,10 @@
 		if(isset($_POST['register_submit']))
 		{
 			$username=$_POST['reg_username'];
+			$emailId=$_POST['email_id'];
+			$mobNo=$_POST['mobile_no'];
 			$pass=$_POST['reg_password'];
+			/*$confirmpass=$_POST['confirm_password'];*/
 			$check_sql="select * from login_info where username='$username'";
 			$check_res=mysqli_query($conn,$check_sql);
 			if(mysqli_num_rows($check_res)>0)
@@ -99,5 +140,4 @@
 		}
 	
 	?>
-
 </html>
